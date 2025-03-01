b095837a268399346667d27b8cf3f76f039f8d03
Doni <vitringr@pm.me>
2025-03-01 16:04:35 +0200

    feat: symbol-usage

--- lua/settings.lua
+++ lua/settings.lua
@@ -37,7 +37,7 @@ o.fillchars = "eob: "         -- End of buffer (default ~) characters.
 
 o.termguicolors = true        -- Enable 24-bit RGB colors.
 
--- vim.opt.shortmess:append("I") -- Disable welcome message.
+vim.opt.shortmess:append("I") -- Disable welcome message.
 
 vim.opt.iskeyword:remove("_") -- Treat "_" as a symbol.
 
